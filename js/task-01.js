befca80fe97e6c99fea6aef12b6b6a2caa2d98bb
NovichenkoYura <novichenko.y@gmail.com>
2022-05-03 19:05:13 +0300

    task1

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,25 @@
-const quantityLiEl = document.querySelectorAll("li.item");
+const quantityLiEl = document.querySelectorAll('li.item');
 console.log('Number of categories:', quantityLiEl.length);
+
+const categoriesEl = document.querySelector('#categories');
+
+const animalItem = categoriesEl.children[0];
+console.log('Category:', animalItem.children[0].textContent);
+
+const animalQuantity = animalItem.querySelectorAll('li');
+console.log('Elements: ', animalQuantity.length);
+
+const productItem = categoriesEl.children[1];
+console.log('Category:', productItem.children[0].textContent);
+
+const productQuantity = productItem.querySelectorAll('li');
+console.log('Elements: ', productQuantity.length);
+
+const technologiesItem = categoriesEl.children[2];
+console.log('Category:', technologiesItem.children[0].textContent);
+
+const technologiesQuantity = technologiesItem.querySelectorAll('li');
+console.log('Elements: ', technologiesQuantity.length);
+
+
 
