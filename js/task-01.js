d23ac58dfc06b49056953ff0767a0f5439215b7e
NovichenkoYura <novichenko.y@gmail.com>
2022-05-05 11:30:41 +0300

    1

--- js/task-01.js
+++ js/task-01.js
@@ -1,25 +1,37 @@
-const quantityLiEl = document.querySelectorAll('li.item');
-console.log('Number of categories:', quantityLiEl.length);
+const itemMasEl = document.querySelectorAll('li.item');
+console.log('Number of categories:', itemMasEl.length);
 
-const categoriesEl = document.querySelector('#categories');
+// for (let i = 0; i < itemMasEl.length; i += 1){
+//     // console.log(itemMasEl[i].firstElementChild.textContent);
+//     console.log(itemMasEl[i].lastElementChild);
+// }
 
-const animalItem = categoriesEl.children[0];
-console.log('Category:', animalItem.children[0].textContent);
 
-const animalQuantity = animalItem.querySelectorAll('li');
-console.log('Elements: ', animalQuantity.length);
+itemMasEl.forEach((item) => console.log('Category:', item.firstElementChild.textContent, 
+'Elements:', item.lastElementChild))
 
-const productItem = categoriesEl.children[1];
-console.log('Category:', productItem.children[0].textContent);
 
-const productQuantity = productItem.querySelectorAll('li');
-console.log('Elements: ', productQuantity.length);
+// item.lastElementChild.length
 
-const technologiesItem = categoriesEl.children[2];
-console.log('Category:', technologiesItem.children[0].textContent);
+// const categoriesEl = document.querySelector('#categories');
 
-const technologiesQuantity = technologiesItem.querySelectorAll('li');
-console.log('Elements: ', technologiesQuantity.length);
+// const animalItem = categoriesEl.children[0];
+// console.log('Category:', animalItem.children[0].textContent);
+
+// const animalQuantity = animalItem.querySelectorAll('li');
+// console.log('Elements: ', animalQuantity.length);
+
+// const productItem = categoriesEl.children[1];
+// console.log('Category:', productItem.children[0].textContent);
+
+// const productQuantity = productItem.querySelectorAll('li');
+// console.log('Elements: ', productQuantity.length);
+
+// const technologiesItem = categoriesEl.children[2];
+// console.log('Category:', technologiesItem.children[0].textContent);
+
+// const technologiesQuantity = technologiesItem.querySelectorAll('li');
+// console.log('Elements: ', technologiesQuantity.length);
 
 
 
