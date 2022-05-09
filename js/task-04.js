06db9e157cdf6c19722f72d2a76d3d6fd18679e0
NovichenkoYura <novichenko.y@gmail.com>
2022-05-09 18:55:40 +0300

    task 4 - done but not correct

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,19 @@
-const targetBtn = document.querySelector('')
+
+
+const incrementListenBtn = document.querySelector('increment');
+const decrementListenBtn = document.querySelector('decrement');
+const counterValue = 0;
+
+
+function onIncrementButtonClick () {
+    counterValue += 1;
+    document.querySelector('#value').textContent = counterValue;
+}
+
+function onDecrementButtonClick () {
+    counterValue -= 1;
+    document.querySelector('#value').textContent = counterValue;
+}
+
+incrementListenBtn.addEventListener('click', onIncrementButtonClick);
+decrementListenBtn.addEventListener('click', onDecrementButtonClick);
