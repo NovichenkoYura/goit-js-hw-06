b668f3c30187465f2bed9bfa18f89fd4b7b19507
NovichenkoYura <novichenko.y@gmail.com>
2022-05-12 19:32:19 +0300

    task9 - done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  bodyBgc: document.querySelector('.widget'),
+  onChangeColorBtn: document.querySelector('.change-color'),
+  onChangeColorText: document.querySelector('.color'),
+
+};
+
+refs.onChangeColorBtn.addEventListener('click', onClickChangeColor);
+
+function onClickChangeColor() {
+  refs.bodyBgc.style.backgroundColor = getRandomHexColor();
+  refs.onChangeColorText.textContent = getRandomHexColor();
+
+}
+
