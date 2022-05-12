712bc129133d808ae15b87be1d3baa843a3dd8f5
NovichenkoYura <novichenko.y@gmail.com>
2022-05-12 12:11:05 +0300

    task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,33 @@
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault()
+
+    const formElements = event.currentTarget.elements;
+
+    // console.log(formElements);
+
+    const mail = formElements.email.value;
+    const password = formElements.password.value;
+
+    if (mail === '' || password === '') {
+        console.log('Alert, все поля должны быть заполнены.')
+        
+    }
+
+    const formData = {
+        mail,
+        password,
+    }
+
+    console.log(formData);
+
+    form.reset()
+}
+
+
+// Если в форме есть незаполненные поля, выводи alert с предупреждением о том, что все поля должны быть заполнены.
+
 
