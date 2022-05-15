a794e58def5bfb6947790d7d9eefa916eef2184b
NovichenkoYura <novichenko.y@gmail.com>
2022-05-15 15:51:53 +0300

    task10

--- js/task-10.js
+++ js/task-10.js
@@ -3,35 +3,60 @@ function getRandomHexColor() {
 }
 
 const refs = {
-  boxesList: document.querySelector('.boxes'),
+  boxesList: document.querySelector('#boxes'),
   inputAmount: document.querySelector('input'),
-  createBtn: document.querySelector('button[data-create]'),
-  destroyBtn: document.querySelector('button[data-destroy]'),
-
-
+  createBtn: document.querySelector("button[data-create]"),
+  destroyBtn: document.querySelector("button[data-destroy]"),
 } 
 
-let firstBoxSize = 30;
+refs.createBtn.addEventListener('click', inputAmountChange);
+refs.destroyBtn.addEventListener('click', clearBoxes);
+
+function inputAmountChange(event) {
+  let amount = Number(refs.inputAmount.value);
+  createBoxes(amount);
+}
 
 function createBoxes(amount) {
-  
-  for (let i = 0; i < amount; i++){
+  clearBoxes();
+  let firstBoxSize = 30;
+  for (let i = 0; i < amount; i++) {
     const addBox = document.createElement('div');
     firstBoxSize += 10;
     addBox.style.height = firstBoxSize + "px"
     addBox.style.width = firstBoxSize + "px"
     addBox.style.backgroundColor = getRandomHexColor();
-    refs.boxesList.append(addBox)
+    refs.boxesList.append(addBox);
+        
   }
+}
   
-}
-
-inputAmount.addEventListener('input', createBoxes)
-
-
 function clearBoxes() {
-  boxesList.innerHTML = " ";
+  refs.boxesList.innerHTML = "";
 }
+
+  
+// function createBoxes(amount) {
+//   let firstBoxSize = 30;
+//   for (let i = 0; i < amount; i++){
+//     const addBox = document.createElement('div');
+//     firstBoxSize += 10;
+//     addBox.style.height = firstBoxSize + "px"
+//     addBox.style.width = firstBoxSize + "px"
+//     addBox.style.backgroundColor = getRandomHexColor();
+//     refs.boxesList.append(addBox);
+//   }
+  
+  
+  
+  
+
+// createBtn.addEventListener('input', createBoxes)
+
+
+// function clearBoxes() {
+//   boxesList.innerHTML = " ";
+// }
 
 
 // function inputAmountChange(event) {
